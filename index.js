587cf77a8732f329658119bef311bac2fcc40549
Azhar Ali <dev.darazhar@gmail.com>
2025-01-31 21:41:40 +0530

    adedd live domain for cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ['*', 'http://localhost:3000'],
+  origin: ['*', 'http://localhost:3000',  'https://tablesprint22.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
